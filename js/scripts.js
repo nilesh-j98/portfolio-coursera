003003547c9791143e5d90bf1c2bc2794cbf0e8d
Nilesh Joshi <nileshjoshi800@gmail.com>
2020-09-12 16:03:33 +0530

    Commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({ interval: 1500 });
+    $('#carouselButton').click(function(){
+        if($("#carouselButton").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel("pause");
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }else{
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#reserveButton').click(function(){
+        $('#reserveModal').modal();
+    });
+    $('#loginLink').click(function(){
+        $('#loginModal').modal();
+    });
+});
